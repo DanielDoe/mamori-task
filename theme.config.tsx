a9e3b0cd5761be8393a822762dd81fb122596773
DanielDoe <30290716+DanielDoe@users.noreply.github.com>
2024-11-26 21:27:02 -0600

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Mamori Written Task</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/DanielDoe/mamori-task',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/DanielDoe/mamori-task',
   footer: {
     text: 'Mamori Written Task',
   },
